65de0b1caa0776fc7c9eddac17c642ca3f156dc5
HEun0420 <usialeta@gmail.com>
2024-08-30 15:25:05 +0900

    react

---
+++ 06_redux/03_redux-project/src/modules/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import pokemonReducer from './PokemnonModule';
+
+const rootReducer = combineReducers({
+    pokemonReducer
+});
+
+export default rootReducer;
